332ab217f4f227299c9fde79cda31993a07c4fe4
HkingAuditore <178854663@qq.com>
2020-04-21 10:05:36 +0800

    更新数据库实验

---
+++ FineUI/FineUI.Examples/Lab/Webs/RankingList.aspx
@@ -0,0 +1,158 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RankingList.aspx.cs" Inherits="WebSharp.RankingList"EnableEventValidation="false" %>
+<%@ Import Namespace="System.ComponentModel" %>
+
+<!DOCTYPE html>
+<style>
+    .title { color: #4c4228;  }
+    .custom-confirm-btn {
+        background-color: #826936;
+        border: 2px solid #f36c36;
+        border-radius: 30px;
+        color: #ffffff;
+        width: 100%;
+    }
+
+    .custom-confirm-btn:hover {
+        background-color: #f36c36;
+        border-color: #f36c36;
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px 1px rgba(252, 166, 96, 0.897);
+        color: rgb(255, 255, 255) !important;
+    }
+
+    .custom-confirm-btn:focus {
+        background-color: #f36c36;
+        border-color: #f36c36;
+        color: rgb(255, 255, 255);
+    }
+
+    .custom-confirm-btn:active {
+        background-color: #f36c36;
+        border-color: #f36c36;
+        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px 3px rgba(252, 153, 96, 0.897);
+        color: rgb(255, 255, 255);
+    }
+</style>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="UTF-8"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta content="ie=edge" http-equiv="X-UA-Compatible"/>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+    <meta content="ie=edge" http-equiv="X-UA-Compatible"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="Content/bootstrap.css" rel="stylesheet"/>
+    <link href="Content/bootstrap-theme.css" rel="stylesheet"/>
+    <script src="Content/bootstrap.min.js"></script>
+    <script src="Content/jquery-1.10.2.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Red Dead Redemption</title>
+</head>
+
+<body style="background-image: url(../Resource/RDR.jpg); background-attachment: fixed; margin-top: -0.5%;">
+<form id="form1" runat="server">
+    <div class="container" style="margin-top: 5%; margin-left: 10%; margin-right: 10%; margin-bottom: 5%; background-color: rgb(255, 244, 204); border-radius: 3px; box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 2px 2px 10px 2px rgba(53, 31, 13, 0.897);">
+       <div class="row" style="padding-top: 5%;">
+           <div class="col-lg-2 col-lg-offset-7">
+               <%-- <asp:TextBox ID="Target" runat="server"></asp:TextBox> --%>               <%-- <asp:DropDownList ID="TargetHome" runat="server"> --%>               <%--     <asp:ListItem Value = "SaintDenis">圣丹尼斯</asp:ListItem> --%>               <%--     <asp:ListItem Value = "Rhodes">罗兹</asp:ListItem> --%>               <%--     <asp:ListItem Value = "Valentine">瓦伦丁</asp:ListItem> --%>               <%--     <asp:ListItem Value = "BlackWater">黑水镇</asp:ListItem> --%>               <%--     <asp:ListItem Value = "Strawberry">草莓镇</asp:ListItem> --%>               <%--     <asp:ListItem Value = "BeecherSHope">比彻之愿</asp:ListItem> --%>               <%--     <asp:ListItem Value = "TheHeartLand">大地之心</asp:ListItem> --%>               <%-- </asp:DropDownList> --%>
+               <asp:TextBox ID="SearchTarget" runat="server"></asp:TextBox>
+           </div>
+           <div class="col-lg-2">
+               <asp:Button ID="Search" runat="server" Text="查找" OnClick="Search_OnClick"/>
+               <asp:Button ID="Refresh" runat="server" Text="刷新" OnClick="Refresh_OnClick"/>
+           </div>
+
+       </div>
+        <div class="row" style="padding-top: 5%;">
+        <div class="col-lg-12">
+        <asp:GridView ID="GridViewMain" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="ID" 
+                      ForeColor="#333333" GridLines="None"  HorizontalAlign="Center" OnSelectedIndexChanged="GridView_SelectedIndexChanged" 
+                      onpageindexchanging="GridView_PageIndexChanging"
+                      PageSize="20" 
+                      OnRowDeleting="GridView_RowDeleting" 
+                      OnRowUpdating="GridView_RowUpdating" 
+                      OnRowEditing="GridView_RowEditing" 
+                      OnRowCancelingEdit="GridView_RowCancelingEdit">
+                <AlternatingRowStyle BackColor="White" />
+                <Columns>
+                    <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="True" SortExpression="ID" />
+                    <asp:ImageField DataAlternateTextField="ID" DataImageUrlField="ID" ReadOnly="True"
+                                    HeaderText="Avatar" DataImageUrlFormatString="../Data/Users/Avatars/{0}.jpg" ControlStyle-Height="128px" ControlStyle-Width="128px">
+
+<ControlStyle Height="128px" Width="128px"></ControlStyle>
+
+                    </asp:ImageField>
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" SortExpression="Age" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address">
+                    <ItemStyle Width="100px" Wrap="False" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="Password" HeaderText="Password" SortExpression="Password" />
+                    <asp:TemplateField HeaderText="查看财产">
+                        <ItemTemplate>
+                            <asp:GridView runat="server" AutoGenerateColumns="False" DataKeyNames="BankAccount" DataSourceID="SqlDataSource1" ID="PropertyData"
+                                          OnRowEditing="PropertyData_RowEditing" OnRowCancelingEdit="PropertyData_RowCancelingEdit" OnRowUpdating="PropertyData_RowUpdating">
+                                <Columns>
+                                    <asp:BoundField DataField="BankAccount" HeaderText="BankAccount" ReadOnly="True" SortExpression="BankAccount" />
+                                    <asp:BoundField DataField="Property" HeaderText="Property" SortExpression="Property" />
+                                    <asp:CommandField ButtonType="Button" HeaderText="操作" ShowDeleteButton="True" ShowEditButton="True" ShowHeader="True" />
+                                </Columns>
+                            </asp:GridView>
+                            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConString %>" 
+                                               SelectCommand='<%#GenerateRowSQL(Convert.ToString(DataBinder.Eval(Container.DataItem,"ID")))%>'
+                                               UpdateCommand=' ' DeleteCommand=' '>
+
+                            </asp:SqlDataSource>
+                            <br />
+                            <asp:TextBox ID="NewAccount" runat="server" Visible="False" Width="75px"></asp:TextBox>
+                            <asp:TextBox ID="NewProperty" runat="server" Visible="False" Width="75px"></asp:TextBox>
+                            <asp:Button ID="ButtonConfirm" runat="server" Text="确定" Visible="False" OnClick="ButtonConfirm_OnClick" />
+                            <asp:Button ID="ButtonCancel" runat="server" Text="取消" Visible="False" OnClick="ButtonCancel_OnClick" />
+                            <asp:Button ID="Button1" runat="server" Text="新增" Visible="True" OnClick="Button1_OnClick"  CommandArgument='<%# DataBinder.Eval(Container.DataItem,"ID") %>'/>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:CommandField ButtonType="Button" HeaderText="数据操作" ShowDeleteButton="True" ShowEditButton="True" />
+                </Columns>
+                <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+                <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+                <SortedAscendingCellStyle BackColor="#FDF5AC" />
+                <SortedAscendingHeaderStyle BackColor="#4D0000" />
+                <SortedDescendingCellStyle BackColor="#FCF6C0" />
+                <SortedDescendingHeaderStyle BackColor="#820000" />
+            </asp:GridView>
+        </div>
+       </div>
+        <%-- <div class="row" style="text-align: center; margin-top: 5%; margin-left: 30%;margin-bottom: 5%"> --%>
+        <%--     <div class="col-lg-12"> --%>
+        <%--         <asp:DetailsView ID="DetailsView" runat="server" Height="50px" Width="259px" AllowPaging="True" AutoGenerateRows="False" CellPadding="4" DataKeyNames="ID"  ForeColor="#333333" GridLines="None"> --%>
+        <%--             <AlternatingRowStyle BackColor="White" /> --%>
+        <%--             <CommandRowStyle BackColor="#FFFFC0" Font-Bold="True" /> --%>
+        <%--             <FieldHeaderStyle BackColor="#FFFF99" Font-Bold="True" /> --%>
+        <%--             <Fields> --%>
+        <%--                 <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="True" SortExpression="ID" /> --%>
+        <%--                 <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" /> --%>
+        <%--                 <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" /> --%>
+        <%--                 <asp:BoundField DataField="Age" HeaderText="Age" SortExpression="Age" /> --%>
+        <%--                 <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" /> --%>
+        <%--                 <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" /> --%>
+        <%--                 <asp:BoundField DataField="Password" HeaderText="Password" SortExpression="Password" /> --%>
+        <%--                 <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" ShowInsertButton="True" /> --%>
+        <%--             </Fields> --%>
+        <%--             <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" /> --%>
+        <%--             <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" /> --%>
+        <%--             <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" /> --%>
+        <%--             <RowStyle BackColor="#FFFBD6" ForeColor="#333333" /> --%>
+        <%--         </asp:DetailsView> --%>
+        <%--     </div> --%>
+        <%-- </div> --%>
+    </div>
+</form>
+
+</body>
+</html>
---
+++ FineUI/FineUI.Examples/Lab/Webs/RankingList.aspx.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSharp
+{
+    public partial class RankingList : System.Web.UI.Page
+    {
+        public static readonly string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+
+        private void UpdateData(string target)
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                var dataSet = new DataSet();
+
+                var sqlDataAdapter = new SqlDataAdapter();
+                var sqlCommand = sqlConnection.CreateCommand();
+                if (target != "")
+                    sqlCommand.CommandText = "SELECT * FROM UserBasicInfo WHERE Name LIKE '%" + target + "%'";
+                else
+                    sqlCommand.CommandText = "SELECT * FROM UserBasicInfo";
+                sqlDataAdapter.SelectCommand = sqlCommand;
+
+                sqlConnection.Open();
+                sqlDataAdapter.Fill(dataSet);
+
+                GridViewMain.DataSource = dataSet;
+                GridViewMain.DataBind();
+                // GridView propertyData = (GridView)(GridView1.Rows[parent_index].FindControl("ChildGridView"));
+
+            }
+
+        }
+
+        private void TestTransaction()
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                sqlConnection.Open();
+                // 新建一个事务
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction("TestTransaction");
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                try
+                {
+                    // 指令1
+                    sqlCommand.CommandText =
+                        "INSERT INTO UserBasicInfo VALUES ('Ezio', 'Ezio Auditore', '男', '31','mail@mail.com','BlackWater','123')";
+                    sqlCommand.ExecuteNonQuery();
+                    // 建立一个保存点
+                    sqlTransaction.Save("Ezio");
+
+                    // 不能执行的指令2
+                    try
+                    {
+                        sqlCommand.CommandText =
+                            "INSERT INTO UserBasicInfo VALUES ('Hking', 'Hking Auditore', '男', '20','mail@mail.com','Strawberry','123','123')";
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        //回滚到保存点
+                        sqlTransaction.Rollback("Ezio");
+                    }
+                    
+                    //提交操作
+                    sqlTransaction.Commit();
+                    Response.Write("<script>alert('SUCCESS!')</script>");
+                }
+                catch
+                {
+                    Response.Write("<script>alert('ERROR!')</script>");
+                }
+
+            }
+        }
+
+        private void TestCommand()
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                // 使用了Target占位符表示目标ID
+                SqlCommand sqlCommand = new SqlCommand("UPDATE UserBasicInfo SET Password = '123456' WHERE ID = @Target", sqlConnection);
+                // 构造Parameter对象
+                SqlParameter targetSqlParameter = new SqlParameter("@Target",SqlDbType.VarChar,200);
+                targetSqlParameter.Value = "Ezio";
+                sqlCommand.Parameters.Add(targetSqlParameter);
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+
+        }
+
+        private void UseCommand(String command)
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                // 使用了Target占位符表示目标ID
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+
+        }
+
+        private void TestCommandProcedure()
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                //设定以存储过程解释CommandText
+                sqlCommand.CommandText = "TestProcedure";
+                // 构造Parameter对象
+                SqlParameter targetSqlParameter = new SqlParameter("@Target",SqlDbType.VarChar,200);
+                targetSqlParameter.Value = "Ezio";
+                sqlCommand.Parameters.Add(targetSqlParameter);
+
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+            }
+
+        }
+
+        private void TestCommandAsyn()
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                //设定以存储过程解释CommandText
+                sqlCommand.CommandText = "TestProcedure";
+                // 构造Parameter对象
+                SqlParameter targetSqlParameter = new SqlParameter("@Target",SqlDbType.VarChar,200);
+                targetSqlParameter.Value = "Ezio";
+                sqlCommand.Parameters.Add(targetSqlParameter);
+
+                sqlConnection.Open();
+
+                IAsyncResult result = sqlCommand.BeginExecuteNonQuery();
+                // 在未完成时弹出WAIT框
+                while (!result.IsCompleted)
+                {
+                    Response.Write("<script>alert('WAIT')</script>");
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                // 在完成后弹出结果（影响行数）
+                Response.Write("<script>alert('Affected "+ sqlCommand.EndExecuteNonQuery(result) + " rows')</script>");
+                sqlConnection.Close();
+            }
+
+        }
+
+        private void TestBuilder()
+        {
+            using (var sqlConnection = new SqlConnection(ConString))
+            {
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+                sqlDataAdapter.SelectCommand =
+                    new SqlCommand(
+                        "SELECT * FROM UserBasicInfo WHERE Gender = '女'",
+                        sqlConnection);
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+
+                DataSet dataSet = new DataSet();
+                sqlDataAdapter.Fill(dataSet,"UserBasicInfo");
+
+                // 添加数据
+                DataTable dataTable = dataSet.Tables["USerBasicInfo"];
+                DataRow dataRow = dataTable.NewRow();
+                dataRow["ID"] = "Ezio";
+                dataRow["Name"] = "Ezio Auditore";
+                dataTable.Rows.Add(dataRow);
+
+                Response.Write("<script>alert('COMMAND: " + sqlCommandBuilder.GetInsertCommand().CommandText + "')</script>");
+                sqlDataAdapter.Update(dataSet, "UserBasicInfo");
+                GridViewMain.DataSource = dataSet.Tables["UserBasicInfo"];
+                GridViewMain.DataBind();
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // UpdateData(!IsPostBack ? "" : TargetHome.SelectedValue);
+            if(!IsPostBack)
+            {
+                UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+            }
+            // else
+            // {
+            //     UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+            // }
+           
+        }
+
+        protected void Refresh_OnClick(object sender, EventArgs e)
+        {
+            UpdateData("");
+        }
+
+        protected string GetString() => "TEST！";
+
+        protected string GenerateRowSQL(string curId) =>
+            "SELECT [BankAccount], [AccountOwnerID], [Property] FROM [BankAccountInfo] WHERE [AccountOwnerID] = '" +
+            curId +"'";
+
+        protected void GridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string sqlStr = "DELETE FROM UserBasicInfo WHERE ID= '" + GridViewMain.Rows[e.RowIndex].Cells[0].Text.ToString() + " '";
+            UseCommand(sqlStr);
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+
+        }
+        protected void GridView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string ID = GridViewMain.Rows[e.RowIndex].Cells[0].Text.ToString();
+            string Name = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[2].Controls[0])).Text.ToString().Trim();
+            string Gender = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[3].Controls[0])).Text.ToString().Trim();
+            string Age = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[4].Controls[0])).Text.ToString().Trim();
+            string Email = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[5].Controls[0])).Text.ToString().Trim();
+            string Address = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[6].Controls[0])).Text.ToString().Trim();
+            string Password = ((TextBox)(GridViewMain.Rows[e.RowIndex].Cells[7].Controls[0])).Text.ToString().Trim();
+            string sqlStr = "UPDATE UserBasicInfo SET Name='" + Name + "',Gender='" + Gender + "',Age='" + Age + "',Email='" + Email + "',Address='" + Address + "',Password='" + Password + "'WHERE ID='" + ID + "'";
+            Response.Write("<script>alert('SQL: " + sqlStr + "')</script>");
+            UseCommand(sqlStr);
+            
+            // UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+        protected void GridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewMain.EditIndex = e.NewEditIndex;
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void GridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewMain.SetPageIndex(GridViewMain.SelectedIndex);
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void GridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewMain.EditIndex = -1;
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void GridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridViewMain.PageIndex = e.NewPageIndex;
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void PropertyData_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            ((GridView)sender).EditIndex = e.NewEditIndex;
+            // UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void PropertyData_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            ((GridView)sender).EditIndex = -1;
+            // UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void PropertyData_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string BankAccount = ((GridView)sender).Rows[e.RowIndex].Cells[0].Text.ToString();
+            string Property = ((TextBox)(((GridView)sender).Rows[e.RowIndex].Cells[1].Controls[0])).Text.ToString().Trim();
+            string sqlStr = "UPDATE BankAccountInfo SET Property='" + Property + "'WHERE BankAccount='" + BankAccount + "'";
+            UseCommand(sqlStr);
+            // UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void Button1_OnClick(object sender, EventArgs e)
+        {
+            string curId = ((Button) sender).CommandArgument.ToString();
+            int index = ((GridViewRow)((Button) sender).NamingContainer).RowIndex;
+            ((Button) GridViewMain.Rows[index].FindControl("ButtonConfirm")).Visible = true;
+            ((Button) GridViewMain.Rows[index].FindControl("ButtonCancel")).Visible = true;
+            ((Button)GridViewMain.Rows[index].FindControl("Button1")).Visible = false;
+
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewAccount")).Visible = true;
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewProperty")).Visible = true;
+        }
+
+        protected void ButtonConfirm_OnClick(object sender, EventArgs e)
+        {
+            int index = ((GridViewRow)((Button)sender).NamingContainer).RowIndex;
+            string curId = GridViewMain.Rows[index].Cells[0].Text.ToString();
+
+            string newAccount =  ((TextBox)GridViewMain.Rows[index].FindControl("NewAccount")).Text.ToString();
+            string newProperty = ((TextBox)GridViewMain.Rows[index].FindControl("NewProperty")).Text.ToString();
+
+            string sqlString = "INSERT INTO BankAccountInfo VALUES ('" + newAccount + "', '" + curId + "'," + newProperty + ")";
+
+            UseCommand(sqlString);
+
+            ((Button)GridViewMain.Rows[index].FindControl("ButtonConfirm")).Visible = false;
+            ((Button)GridViewMain.Rows[index].FindControl("ButtonCancel")).Visible = false;
+            ((Button)GridViewMain.Rows[index].FindControl("Button1")).Visible = true;
+
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewAccount")).Visible = false;
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewProperty")).Visible = false;
+
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+
+        protected void ButtonCancel_OnClick(object sender, EventArgs e)
+        {
+
+            int index = ((GridViewRow)((Button)sender).NamingContainer).RowIndex;
+            ((Button)GridViewMain.Rows[index].FindControl("ButtonConfirm")).Visible = false;
+            ((Button)GridViewMain.Rows[index].FindControl("ButtonCancel")).Visible = false;
+            ((Button)GridViewMain.Rows[index].FindControl("Button1")).Visible = true;
+
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewAccount")).Visible = false;
+            ((TextBox)GridViewMain.Rows[index].FindControl("NewProperty")).Visible = false;
+        }
+
+        protected void Search_OnClick(object sender, EventArgs e)
+        {
+            UpdateData(!IsPostBack ? "" : SearchTarget.Text.Trim());
+        }
+    }
+}
